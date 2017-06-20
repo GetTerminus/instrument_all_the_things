fe77314878ded2312a5333b9f9911f1859a20e9c
Brian Malinconico <bmalinconico@terminus.com>
2017-06-19 21:19:05 -0400

    Adding test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pry'
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
 require 'instrument_all_the_things'
 
 RSpec.configure do |config|
