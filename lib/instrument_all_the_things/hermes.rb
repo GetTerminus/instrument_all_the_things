e192a23199be97a7b72de90dd4fe13ef2936b166
stevievines <stevievines@gmail.com>
2017-10-05 14:08:54 -0400

    Remove redundant hermes tag

--- lib/instrument_all_the_things/hermes.rb
+++ lib/instrument_all_the_things/hermes.rb
@@ -3,12 +3,10 @@ module InstrumentAllTheThings
     include HelperMethods
 
     ActiveSupport::Notifications.subscribe('hermes_messenger_of_the_gods.worker.run_job') do |_, start, finish, _, payload|
-      with_tags("hermes_class:#{normalize_class_name(payload[:job].class)}") do
-        if payload[:exception].present?
-          BackendJob.error(job: payload[:job], exception: payload[:exception_object].class)
-        else
-          BackendJob.completed(job: payload[:job], duration: finish - start)
-        end
+      if payload[:exception].present?
+        BackendJob.error(job: payload[:job], exception: payload[:exception_object].class)
+      else
+        BackendJob.completed(job: payload[:job], duration: finish - start)
       end
     end
   end
