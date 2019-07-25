f59e4b9c6b6bf854ba33cc6bbdeec96dcc467229
Brian Malinconico <bmalinconico@terminus.com>
2019-07-25 14:31:47 -0400

    Adding spec support for histogram types

--- lib/instrument_all_the_things/testing/aggregators.rb
+++ lib/instrument_all_the_things/testing/aggregators.rb
@@ -14,6 +14,12 @@ module InstrumentAllTheThings
         )
       end
 
+      def get_histogram(hist_name)
+        BaseAggregator.new(
+          InstrumentAllTheThings.transmitter.histogram[hist_name]
+        )
+      end
+
       class BaseAggregator
         attr_accessor :metrics
 
--- lib/instrument_all_the_things/testing/setup.rb
+++ lib/instrument_all_the_things/testing/setup.rb
@@ -10,8 +10,9 @@ module InstrumentAllTheThings
     end
 
     def reset!
-      self.counts  = Hash.new{|h,k| h[k] = [] }
-      self.timings = Hash.new{|h,k| h[k] = [] }
+      self.counts    = Hash.new{|h,k| h[k] = [] }
+      self.timings   = Hash.new{|h,k| h[k] = [] }
+      self.histogram = Hash.new{|h,k| h[k] = [] }
     end
 
     alias _original_count_old _original_count
@@ -24,6 +25,12 @@ module InstrumentAllTheThings
     def _original_timing(stat, ms, options = {})
       self.timings[stat] << { value: ms, tags: options[:tags] || [] }
       _original_timing_old(stat, ms, options)
+    end
+
+    alias _original_histogram_old _original_histogram
+    def _original_histogram(stat, ms, options = {})
+      self.histograms[stat] << { value: ms, tags: options[:tags] || [] }
+      _original_histogram_old(stat, ms, options)
     end
 
     def send_stats(*_)
