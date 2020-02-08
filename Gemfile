d650af288526ca4d83aa37fa4ea9c8c00468ab74
Brian Malinconico <bmalinconico@terminus.com>
2020-02-07 21:47:30 -0500

    Basline

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in instrument_all_the_things.gemspec
+gemspec
---
+++ lib/instrument_all_the_things.rb
@@ -0,0 +1,6 @@
+require "instrument_all_the_things/version"
+
+module InstrumentAllTheThings
+  class Error < StandardError; end
+  # Your code goes here...
+end
