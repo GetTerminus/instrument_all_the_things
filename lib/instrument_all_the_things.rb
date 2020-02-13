782d06d1119d89401badf21de92e6139210174e4
Brian Malinconico <bmalinconico@terminus.com>
2020-02-13 13:23:05 -0500

    In progress

--- lib/instrument_all_the_things.rb
+++ lib/instrument_all_the_things.rb
@@ -1,6 +1,34 @@
-require "instrument_all_the_things/version"
+# frozen_string_literal: true
+
+require 'instrument_all_the_things/version'
+
+require_relative './instrument_all_the_things/method_proxy'
 
 module InstrumentAllTheThings
   class Error < StandardError; end
-  # Your code goes here...
+
+  module ClassMethods
+    def instrument(trace: true)
+      @last_settings = {
+        trace: trace
+      }
+    end
+
+    def method_added(method_name)
+      return unless @last_settings
+
+      settings = @last_settings
+      @last_settings = nil
+
+      InstrumentAllTheThings::MethodProxy
+        .for_class(self)
+        .wrap_implementation(method_name, settings)
+    end
+  end
+
+  def self.included(other_class)
+    other_class.extend(ClassMethods)
+  end
 end
+
+IATT = InstrumentAllTheThings
