cfec51114fbe6ae4799993fa3df60765fec91822
Brian Malinconico <bmalinconico@terminus.com>
2020-02-13 15:47:30 -0500

    Missing files

---
+++ lib/instrument_all_the_things/clients/stat_reporter/datadog.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'datadog/statsd'
+
+module InstrumentAllTheThings
+  module Clients
+    module StatReporter
+      class DataDog < Datadog::Statsd
+        %i{
+        count
+        decrement
+        distribution
+        gauge
+        histogram
+        increment
+        set
+        time
+        timing
+        }.each do |meth|
+          define_method(meth) do |*args, **kwarg, &blk|
+            args[0] = stat_prefix + args[0]
+            super(*args, **kwarg, &blk)
+          end
+        end
+
+        private
+
+        def stat_prefix
+          'omg'
+        end
+      end
+    end
+  end
+end
