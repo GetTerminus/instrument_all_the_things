10b0b63e32d10fcef705d0977f91d18954e517c3
Brian Malinconico <bmalinconico@terminus.com>
2020-08-27 08:59:10 -0400

    Upgrade gems & add 2.7

--- lib/instrument_all_the_things/instrumentors/execution_count_and_timing.rb
+++ lib/instrument_all_the_things/instrumentors/execution_count_and_timing.rb
@@ -8,12 +8,12 @@ module InstrumentAllTheThings
       proc do |klass, next_blk, actual_code|
         context.tags ||= []
 
-        InstrumentAllTheThings.increment("#{context.stats_name(klass)}.executed", { tags: context.tags })
-        InstrumentAllTheThings.time("#{context.stats_name(klass)}.duration", { tags: context.tags }) do
+        InstrumentAllTheThings.increment("#{context.stats_name(klass)}.executed", tags: context.tags)
+        InstrumentAllTheThings.time("#{context.stats_name(klass)}.duration", tags: context.tags) do
           next_blk.call(klass, actual_code)
         end
       rescue StandardError
-        InstrumentAllTheThings.increment("#{context.stats_name(klass)}.errored", { tags: context.tags })
+        InstrumentAllTheThings.increment("#{context.stats_name(klass)}.errored", tags: context.tags)
         raise
       end
     end
