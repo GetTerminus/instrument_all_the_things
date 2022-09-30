c0f6bac1b355ddc175dfa6d7ca412e093330cc08
nrowegt <1641120+nrowegt@users.noreply.github.com>
2022-09-30 07:13:13 -0400

    bundle update and fix no active trace bug

--- lib/instrument_all_the_things/thread.rb
+++ lib/instrument_all_the_things/thread.rb
@@ -6,12 +6,18 @@ require 'ddtrace'
 class Thread
   def self.new_traced
     trace = Datadog::Tracing.active_trace
-    trace_digest = trace.to_digest
 
-    Thread.new do |*args|
-       # Inherits trace properties from the trace digest
-      Datadog::Tracing.trace(trace.name, continue_from: trace_digest) do |_span, trace|
-        trace.id == trace_digest.trace_id
+    if trace
+      trace_digest = trace.to_digest
+      Thread.new do |*args|
+         # Inherits trace properties from the trace digest
+        Datadog::Tracing.trace(trace.name, continue_from: trace_digest) do |_span, trace|
+          trace.id == trace_digest.trace_id
+          yield(*args)
+        end
+      end
+    else
+      Thread.new do |*args|
         yield(*args)
       end
     end
